62c0035d121101cf602d115c6e4ff2730ccd316d
rybaier <rybaier@gmail.com>
2022-01-21 12:46:15 -0800

    routes and links connectivity test passed

--- project-react/src/components/pagecomponents/NavigationPanel.js
+++ project-react/src/components/pagecomponents/NavigationPanel.js
@@ -7,7 +7,7 @@ const NavigationPanel = () => {
     <Card className="nav-panel">
         <Navbar color="green" dark light expand="md" >
             {/* Wrap these in Links? to utilize react router dom*/}
-                <NavLink className= 'links' href="/components/Home"> Home </NavLink>
+                <NavLink className= 'links' href="/"> Home </NavLink>
                  <NavLink className= 'links' href="/components/Quotes"> Quotes </NavLink>
                  <NavLink className= 'links' href="/components/Characters"> Characters </NavLink>
                  <NavLink className= 'links' href="/components/Movies"> Movies </NavLink>
